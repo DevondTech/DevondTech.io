1cfa5898a505277c1d9a4cf66db3d27c7aa969bc
meprasetyo <me.serj.adam@gmail.com.com>
2020-07-25 20:32:44 +0700

    update data 25 juli 2020

---
+++ durian/back/js/durian.js
@@ -0,0 +1,68 @@
+var renderPage = true;
+$(window).on("load", function(){
+  if(renderPage) {
+    	$('body').addClass('loaded');
+      $('#product').load("product.html");  
+      /*$('#next').load("next-button.html");*/   
+  }         
+}); 
+
+$(document).ready(function(){
+ 	$('#chatSend').on('click',function(e){
+    var chatText = $('#chat-text').val(); 
+    /*console.log(chatText);*/
+    $('#output-chat').html(chatText);
+  });
+});
+
+$('#openForm').on('click',function(e){
+  $('#myForm').attr('style','display:block'); 
+  $('#open-chat-modal').attr('style','display:none'); 
+});
+
+$('#closeForm').on('click',function(e){
+  $('#myForm').attr('style','display:none'); 
+  $('#open-chat-modal').attr('style','display:block'); 
+});
+
+$('#next-page').on('click',function(e){
+  $('#product').load("next-page.html");   
+  $('#next').attr('style','display:none');  
+  $('#back').attr('style','display:block');   
+})
+
+$('#back-page').on('click',function(e){
+  $('#product').load("product.html");   
+  $('#next').attr('style','display:block');  
+  $('#back').attr('style','display:none');  
+})
+
+$('#next').mouseover(function(){
+  $('#imgNext1').css('display','none');
+   $('#imgNext2').css('display','none');
+   $('#imgNext1Hover').css('display','block');
+   $('#imgNext2Hover').css('display','block');
+});
+
+$('#next').mouseout(function(){
+  $('#imgNext1').css('display','block');
+  $('#imgNext2').css('display','block');
+  $('#imgNext1Hover').css('display','none');
+  $('#imgNext2Hover').css('display','none');
+});
+
+$('#back').mouseover(function(){
+  $('#imgBack1').css('display','none');
+  $('#imgBack2').css('display','none');
+  $('#imgBack1Hover').css('display','block');
+  $('#imgBack2Hover').css('display','block');
+});
+
+$('#back').mouseout(function(){
+  $('#imgBack1').css('display','block');
+  $('#imgBack2').css('display','block');
+  $('#imgBack1Hover').css('display','none');
+  $('#imgBack2Hover').css('display','none');
+});
+
+
