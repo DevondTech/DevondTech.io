f9bff7dcdfbc0226fe3bd7c3c0b8f805783df047
meprasetyo <me.serj.adam@gmail.com.com>
2020-08-03 20:45:40 +0700

    update template

---
+++ baruga/js/baruga.js
@@ -0,0 +1,48 @@
+function myFunction(x) {
+  if (x.matches) { 
+    $('#top1-ESikola').attr('class','padding-LeftRight40 col-lg-12 col-md-12 col-sm-12');
+  } else {
+    $('#top1-ESikola').attr('class','padding-LeftRight40 col-lg-5 col-md-5 col-sm-5');
+  }
+}
+
+var x = window.matchMedia("(max-width: 900px)")
+myFunction(x) 
+x.addListener(myFunction) 
+
+
+/*slide right or left*/
+
+$('#carouselSmall').on('slide.bs.carousel', function (e) {
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  var itemsPerSlide = 3;
+  var totalItems = $('.carousel-item').length;
+  
+  if (idx >= totalItems-(itemsPerSlide-1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i=0; i<it; i++) {
+          // append slides to end
+          if (e.direction=="left") {
+              $('.carousel-item').eq(i).appendTo('.carousel-inner');
+          }
+          else {
+              $('.carousel-item').eq(0).appendTo('.carousel-inner');
+          }
+      }
+  }
+});
+
+$(document).ready(function() {
+/* show lightbox when clicking a thumbnail */
+  $('a.thumb').click(function(event){
+    event.preventDefault();
+    var content = $('.modal-body');
+    content.empty();
+      var title = $(this).attr("title");
+      $('.modal-title').html(title);        
+      content.html($(this).html());
+      $(".modal-profile").modal({show:true});
+  });
+
+});
